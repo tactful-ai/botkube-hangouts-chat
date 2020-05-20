2a336c1e659f3c44c6542f1446611ad1512a2afd
Mustafa Saber <mostafa.saber3789@gmail.com>
2020-05-20 17:09:09 +0200

    script to deploy function on aws

---
+++ deploy_function.sh
@@ -0,0 +1,12 @@
+set -e
+set -x
+
+if [[ -e botkube_hangouts_function.zip ]]; then
+    rm botkube_hangouts_function.zip
+fi
+
+zip -r botkube_hangouts_function.zip . -x '*.git*' deploy_function.sh response_example.json
+
+aws lambda update-function-code --function-name botkube_hangouts --zip-file fileb://botkube_hangouts_function.zip
+
+rm botkube_hangouts_function.zip
