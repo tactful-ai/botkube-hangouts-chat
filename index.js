1c1d9be7b01bcc4ad1da631c3eb923277655d465
Mustafa Saber <mostafa.saber3789@gmail.com>
2020-05-27 11:23:53 +0200

    make lambda send to hangouts

--- index.js
+++ index.js
@@ -1,80 +1,47 @@
 exports.handler = async (event) => {
   let body = JSON.parse(event.body);
-  console.log(`The body is ${JSON.stringify(body)}`);
   const fetch = require('node-fetch');
   const jbuilder = require('jbuilder');
-  var bugsnagError = body.error;
-  var bugsnagProject = body.project;
+  var k8s_meta = body.meta;
+  var k8s_status = body.status;
+  var k8s_summary = body.summary;
 
-  console.log(`kind ${body.kind}`);
+  var messageBody = jbuilder.encode(function (json) {
+    json.set('cards', function (json) {
+      json.child(function (json) {
+        json.set('header', function (json) {
+          json.set('title', `${k8s_meta.name}`);
+          json.set('subtitle', `${k8s_meta.kind} in namespace ${k8s_meta.namespace}`);
+        });
 
-  // var messageBody = jbuilder.encode(function (json) {
-  //   json.set('cards', function (json) {
-  //     json.child(function (json) {
-  //       json.set('header', function (json) {
-  //         json.set('title', `${bugsnagProject.name}`);
-  //         json.set('subtitle', `${bugsnagError.releaseStage}`);
-  //       });
+        json.set('sections', function (json) {
+          json.child(function (json) {
+            json.set('widgets', function (json) {
+              json.child(function (json){
+                json.set('keyValue', function(json) {
+                  json.set('topLabel', `Status type ${k8s_status.type}, Level ${k8s_status.level}`);
+                  json.set('content', `${k8s_summary}`);
+                  json.set('contentMultiline', true);
+                });
+              })
+            });
+          });
+        });
+      });
+    });
+  });
 
-  //       json.set('sections', function (json) {
-  //         json.child(function (json) {
-  //           json.set('widgets', function (json) {
-  //             json.child(function (json){
-  //               json.set('keyValue', function(json) {
-  //                 json.set('topLabel', 'Project Name');
-  //                 json.set('content', `${bugsnagError.exceptionClass} in ${bugsnagError.context} and the error is ${bugsnagError.message}`);
-  //                 json.set('contentMultiline', true);
-  //               });
-  //             })
-  //             json.child(function (json) {
-  //               json.set('buttons', function (json) {
-  //                 json.child(function (json) {
-  //                   json.set('textButton', function (json) {
-  //                     json.set('text', 'VIEW ON BUGSNAG');
-  //                     json.set('onClick', function (json) {
-  //                       json.set('openLink', function (json) {
-  //                         json.set('url', bugsnagError.url);
-  //                       });
-  //                     });
-  //                   });
-  //                 });
+  var options = {
+    // url: process.env.GOOGLE_CHAT_URL,
+    method: 'POST',
+    body: messageBody,
+    headers: {
+      'Content-Type': 'application/json; charset=UTF-8'
+    }
+  };
 
-  //                 if (typeof bugsnagError.createdIssue != "undefined") {
-  //                   json.child(function (json) {
-  //                     json.set('textButton', function (json) {
-  //                       json.set('text', `VIEW ON ${bugsnagError.createdIssue.type.toUpperCase()}`);
-  //                       json.set('onClick', function (json) {
-  //                         json.set('openLink', function (json) {
-  //                           json.set('url', bugsnagError.createdIssue.url);
-  //                         });
-  //                       });
-  //                     });
-  //                   });
-  //                 }
-  //               });
-  //             });
-  //           });
-  //         });
-  //       });
-  //     });
-  //   });
-  // });
-
-
-  // console.log(`Message Body: ${messageBody}`);
-  // console.log(`Message Body String: ${JSON.stringify(messageBody)}`)
-
-  // var options = {
-  //   // url: process.env.GOOGLE_CHAT_URL,
-  //   method: 'POST',
-  //   body: messageBody,
-  //   headers: {
-  //     'Content-Type': 'application/json; charset=UTF-8'
-  //   }
-  // };
-
-  // return fetch(process.env.GOOGLE_CHAT_URL, options)
-  //   .then(res => res.json())
-  //   .then(json => console.log(json));
+  return fetch(process.env.GOOGLE_CHAT_URL, options)
+    .then(res => res.json())
+    .then(json => console.log(json));
 
 };
